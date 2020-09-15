6b0eb220e95868e8e20f71c06d7a495b7287f2db
Katy Rog <katkopikat@gmail.com>
2020-09-15 16:01:47 +0300

    feat: task completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let teamName = [];
+    members.forEach(function (item) {
+      if (typeof (item) === 'string') {
+        teamName.push(item.trim().charAt(0).toUpperCase());
+      }
+    });
+    return teamName.sort().join('');
+  } else {
+    return false;
+  }
+}
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  try {
+    (date.getTime())
+  } catch (error) {
+    throw new Error('THROWN ' + error);
+  }
+
+  const month = date.getMonth();
+  if (month >= 2 && month <= 4) {
+    return "spring";
+  }
+  if (month >= 5 && month <= 7) {
+    return "summer";
+  }
+  if (month >= 8 && month <= 10) {
+    return "fall";
+  }
+  if (month === 11 || month >= 0 && month <= 1) {
+    return "winter";
+  }
+}
