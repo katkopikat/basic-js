899e3f39145f322f14687984711a6483d4773c4e
Katy Rog <katkopikat@gmail.com>
2020-09-16 00:16:08 +0300

    feat: task carbon-dating completed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const k = 0.693/HALF_LIFE_PERIOD;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+module.exports = function dateSample(sampleActivity) {
+  const SAMPLE = parseFloat(sampleActivity);
+  if (SAMPLE == false || typeof (sampleActivity) !== 'string' ) {
+      return false;
+  }
+  let age = Math.ceil((Math.log(MODERN_ACTIVITY / SAMPLE)) / k);
+  return age > 0 ? age: false;
 };
