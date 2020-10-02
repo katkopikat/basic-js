84865d0ac5e067b66c5139b7890c8cb1f54eb530
Katy Rog <katkopikat@gmail.com>
2020-10-02 04:05:39 +0300

    feat: task transform-array complete

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('ERROR');
+  }
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+
+      case '--discard-prev':
+        if (newArr.length > 0 && arr[i - 2] !== "--discard-next") {
+          newArr.pop();
+        } break;
+
+      case '--double-next':
+        if (i < arr.length - 1) {
+          newArr.push(arr[i + 1]);
+        } break;
+
+      case '--double-prev':
+        if (newArr.length > 0 && arr[i - 2] !== "--discard-next") {
+          newArr.push(arr[i - 1]);
+        } break;
+
+      default:
+        newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
