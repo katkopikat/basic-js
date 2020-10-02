a3943de539b8412794377140ff68dca551c6f02a
Katy Rog <katkopikat@gmail.com>
2020-10-02 18:14:28 +0300

    feat: task 'sipmle-chain' complete

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,40 @@
 const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  arr: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) this.arr.push(``);
+    this.arr.push(`${value}`);
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (typeof position === 'number' || 
+      this.arr.indexOf(`${position}`) !== -1) {
+      this.arr.splice(position - 1, 1)
+      return this;
+
+    } else {
+      this.arr = [];
+      throw new Error('ERROR');
+    }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.arr.map(el => `( ${el} )`).join('~~');
+    this.arr = [];
+    return result;
   }
+
 };
 
 module.exports = chainMaker;
