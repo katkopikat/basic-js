535593f336ca8f5be8c98d23239822859354e01f
Katy Rog <katkopikat@gmail.com>
2020-10-04 01:01:26 +0300

    feat: task vigenere-cipher copmplete

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction) {
+    this.direction = direction === undefined ? true : direction;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+
+  encrypt(str, key) {
+    return this.crypt(str, key, 'encrypt');
+  }
+
+  decrypt(str, key) {
+    return this.crypt(str, key, 'decrypt');
+  }
+
+  crypt(message, key, mode) {
+
+    message = message.toUpperCase();
+    let keyRepeat = key.toUpperCase().repeat(Math.ceil(message.length / key.length));
+    let cryptMessage = '',
+      k = '',
+      charCount = 0;
+
+    if (!message || !key) throw new Error('ERROR');
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/)) {
+        k += keyRepeat[charCount];
+
+        if (mode === 'encrypt') {
+          cryptMessage += this.alphabet[(this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(k[i])) % 26]
+        } else {
+          let char = this.alphabet.indexOf(message[i]) - this.alphabet.indexOf(k[i]);
+          if (char < 0) char += 26;
+          cryptMessage += this.alphabet[char]
+        }
+        charCount++;
+      } else {
+        k += message[i];
+        cryptMessage += message[i];
+      }
+    }
+    return this.direction ? cryptMessage : cryptMessage.split('').reverse().join('');
   }
 }
+
 
 module.exports = VigenereCipheringMachine;
